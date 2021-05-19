4c337d8c07105f9dc51a795acce6b9a76c5c6eed
seois830926 <idmahoro@gmail.com>
2021-05-19 11:31:29 +0900

    first commit - button

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <VueButton text="Button1"></VueButton>
+    <VueButton text="Button2" icon-class="fa-star"></VueButton>
+    <VueButton text="Button3" icon-class="fa-plus"></VueButton>
+  </div>
+</template>
+
+<script>
+import VueButton from './components/VueButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VueButton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/VueButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button>
+        <span v-if="iconClass && iconClass.length > 0" class="fa" :class="iconClass"></span>
+        {{text}}
+     </button>
+</template>
+
+<script>
+export default {
+  name: 'VueButton',
+  props: {
+    text: String,
+    iconClass: String
+  },
+  mounted() {
+      console.log(this.iconClass)
+  }
+}
+</script>
